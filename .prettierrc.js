d6b4951b06f975b17c8aa6aa7d0030cfe3a31632
zhm <frontzhm@163.com>
2025-12-10 12:27:26 +0800

    feat: add lints

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100, // 每行最大字符数
+  tabWidth: 2, // 缩进宽度
+  useTabs: false, // 使用空格而非制表符
+  singleQuote: true, // 单引号
+  semi: true, // 句末分号
+  trailingComma: 'es5', // 尾逗号（ES5 兼容）
+  bracketSpacing: true, // 对象字面量括号间空格
+  arrowParens: 'avoid', // 箭头函数单参数省略括号
+  endOfLine: 'lf', // 行尾换行符
+  proseWrap: 'never', // Markdown 文本不换行
+};
+
